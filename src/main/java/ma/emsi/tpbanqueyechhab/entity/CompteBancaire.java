f6676dde9ddf7189fb048eb37f5c2f052ea57ad5
fz.yech <fatimayechhab@gmail.com>
2023-11-08 22:29:31 +0100

    Écriture d'une entité CompteBancaire

---
+++ src/main/java/ma/emsi/tpbanqueyechhab/entity/CompteBancaire.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tpbanqueyechhab.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author user
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String nom;
+    private int solde;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public CompteBancaire(String nom, int solde, Long id) {
+        this.nom = nom;
+        this.solde = solde;
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ma.emsi.tpbanqueyechhab.entity.CompteBancaire[ id=" + id + " ]";
+    }
+
+    public CompteBancaire(String nom, int solde) {
+        this.nom = nom;
+        this.solde = solde;
+    }
+
+    public void deposer(int montant) {
+        solde += montant;
+    }
+
+    public void retirer(int montant) {
+        if (montant < solde) {
+            solde -= montant;
+        } else {
+            solde = 0;
+        }
+    }
+}
