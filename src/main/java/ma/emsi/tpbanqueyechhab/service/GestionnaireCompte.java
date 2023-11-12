b7980781ad792c3b0499b45185f3b71d5ec82805
fz.yech <fatimayechhab@gmail.com>
2023-11-12 21:34:42 +0100

    Écriture d'un bean CDI GestionnaireCompte

---
+++ src/main/java/ma/emsi/tpbanqueyechhab/service/GestionnaireCompte.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tpbanqueyechhab.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import ma.emsi.tpbanqueyechhab.entity.CompteBancaire;
+
+/**
+ *
+ * @author user
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "fzoxy", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@RequestScoped
+public class GestionnaireCompte {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    public List<CompteBancaire> getAllComptes() {
+        String jpqlQuery = "SELECT c FROM CompteBancaire c";
+        TypedQuery<CompteBancaire> query = em.createQuery(jpqlQuery, CompteBancaire.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof GestionnaireCompte)) {
+            return false;
+        }
+        GestionnaireCompte other = (GestionnaireCompte) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ma.emsi.tpbanqueyechhab.service.GestionnaireCompte[ id=" + id + " ]";
+    }
+
+}
